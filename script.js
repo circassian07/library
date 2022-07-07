9dd7f35ecdfc889c226ca6b2159de94b0b8b2851
Kantemir <kantemir.kertiev@hotmail.com>
2022-07-07 18:52:10 -0400

    added a form

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 let myLibrary = [];
 
+const addBtn = document.getElementById("add_btn")
+const formDiv = document.getElementById("form")
+
 function Book(author, title, length, read) {   // constructor function
    this.author = author;
    this.title = title;
@@ -12,5 +15,16 @@ const Book1 = new Book("Stephen King", "IT", 456, false) //rewrite
 function addBookToLibrary() {
 
 }
+
+const displayForm = addBtn.addEventListener("click", (e) => {
+   if (formDiv.style.display === "none") {
+      formDiv.style.display = "block"
+   } else {
+      formDiv.style.display = "none"
+   }
+   
+
+      
+})
 
 console.log(Book1)
