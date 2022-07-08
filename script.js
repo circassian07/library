488e708f9e0ff29ddfa3db11f9f9eb8356a1f1a6
Kantemir <kantemir.kertiev@hotmail.com>
2022-07-07 20:55:49 -0400

    array populates with created objects

--- script.js
+++ script.js
@@ -1,30 +1,44 @@
 let myLibrary = [];
 
 const addBtn = document.getElementById("add_btn")
+const submitBtn = document.getElementById("sbt_btn")
 const formDiv = document.getElementById("form")
+const mainContainer = document.getElementById("main")
 
-function Book(author, title, length, read) {   // constructor function
+
+
+function Book(author, title, pages, read) {   // constructor function
    this.author = author;
    this.title = title;
-   this.length = length;
+   this.pages = pages;
    this.read = read; 
-}
-
-const Book1 = new Book("Stephen King", "IT", 456, false) //rewrite
-
-function addBookToLibrary() {
-
 }
 
 const displayForm = addBtn.addEventListener("click", (e) => {
    if (formDiv.style.display === "none") {
       formDiv.style.display = "block"
-   } else {
+   } else{
       formDiv.style.display = "none"
    }
-   
-
-      
 })
 
-console.log(Book1)
+const addCard = submitBtn.addEventListener("click", (e) => {
+   mainContainer.innerHTML += `<div class="card"></div>`;
+
+      // input variables
+      let inputAuthor = document.getElementById('author').value
+      let inputTitle = document.getElementById('title').value
+      let inputPages = parseInt(document.getElementById('pages').value)
+      let inputStatus = document.getElementsByName('status').value
+
+      let newBook = new Book(inputAuthor,inputTitle,inputPages,inputStatus)
+      
+      myLibrary.push(newBook)
+
+      console.log(myLibrary)
+   })
+
+function addBookToLibrary() {
+
+}
+
