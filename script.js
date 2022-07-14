3770fa094c73c9f9e0f7842d28876c1ebb7bcf63
Kantemir <kantemir.kertiev@hotmail.com>
2022-07-14 19:25:07 -0400

    added functioning delete btns to cards

--- script.js
+++ script.js
@@ -6,39 +6,61 @@ const formDiv = document.getElementById("form")
 const mainContainer = document.getElementById("main")
 
 
-
-function Book(author, title, pages, read) {   // constructor function
+   // constructor function
+function Book(author, title, pages, read) {   
    this.author = author;
    this.title = title;
    this.pages = pages;
    this.read = read; 
 }
 
+   // calls up a form to add new objects
 const displayForm = addBtn.addEventListener("click", (e) => {
    if (formDiv.style.display === "none") {
       formDiv.style.display = "block"
-   } else{
+   } else {
       formDiv.style.display = "none"
    }
 })
 
+   // on Submit button click takes input and sends it to the constructor
 const addCard = submitBtn.addEventListener("click", (e) => {
-   mainContainer.innerHTML += `<div class="card"></div>`;
-
       // input variables
       let inputAuthor = document.getElementById('author').value
       let inputTitle = document.getElementById('title').value
       let inputPages = parseInt(document.getElementById('pages').value)
       let inputStatus = document.getElementsByName('status').value
-
+      // creates new objects using the contstructor
       let newBook = new Book(inputAuthor,inputTitle,inputPages,inputStatus)
-      
+      // adds the new object to array
       myLibrary.push(newBook)
-
       console.log(myLibrary)
+
+      addBookToLibrary()
    })
 
-function addBookToLibrary() {
 
+   // populates the grid with objects from the array
+function addBookToLibrary() {
+   mainContainer.innerHTML = " ";
+
+   for (let i=0;i<myLibrary.length; i++) {
+      mainContainer.innerHTML += 
+         `<div class="card" id="card${i}">
+            <p>Author: ${myLibrary[i].author}</p>
+            <p>Title: ${myLibrary[i].title}</p>
+            <p>Pages: ${myLibrary[i].pages}</p>
+            <p>Read: ${myLibrary[i].read}</p>
+            <button type="button" id="del${i}" onclick="deleteCard(${i})"">Delete</button>
+         </div>`;     
+      }
 }
 
+
+   // accepts the card number from the respective delete button and deletes the object from the array
+function deleteCard(i) {
+   console.log('click');
+   myLibrary.pop(i);
+   addBookToLibrary();
+} 
+      
