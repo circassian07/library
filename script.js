8900aafd68663b3a9f43d7415ba69b0d67339c3c
Kantemir <kantemir.kertiev@hotmail.com>
2022-07-14 20:32:16 -0400

    added styling to cards

--- script.js
+++ script.js
@@ -25,11 +25,13 @@ const displayForm = addBtn.addEventListener("click", (e) => {
 
    // on Submit button click takes input and sends it to the constructor
 const addCard = submitBtn.addEventListener("click", (e) => {
+   // prevents page from refreshing on btn click
+   e.preventDefault();
       // input variables
       let inputAuthor = document.getElementById('author').value
       let inputTitle = document.getElementById('title').value
       let inputPages = parseInt(document.getElementById('pages').value)
-      let inputStatus = document.getElementsByName('status').value
+      let inputStatus = radioValue();
       // creates new objects using the contstructor
       let newBook = new Book(inputAuthor,inputTitle,inputPages,inputStatus)
       // adds the new object to array
@@ -37,6 +39,8 @@ const addCard = submitBtn.addEventListener("click", (e) => {
       console.log(myLibrary)
 
       addBookToLibrary()
+      
+      document.getElementById("myForm").reset();
    })
 
 
@@ -51,7 +55,7 @@ function addBookToLibrary() {
             <p>Title: ${myLibrary[i].title}</p>
             <p>Pages: ${myLibrary[i].pages}</p>
             <p>Read: ${myLibrary[i].read}</p>
-            <button type="button" id="del${i}" onclick="deleteCard(${i})"">Delete</button>
+            <button type="button" class="dlt_btn" id="del${i}" onclick="deleteCard(${i})"">Delete</button>
          </div>`;     
       }
 }
@@ -60,7 +64,18 @@ function addBookToLibrary() {
    // accepts the card number from the respective delete button and deletes the object from the array
 function deleteCard(i) {
    console.log('click');
-   myLibrary.pop(i);
+   myLibrary.splice(i, 1);
    addBookToLibrary();
 } 
+
+function radioValue () {
+   let readStatus = document.getElementsByName('status')
+   for (i=0;i<readStatus.length;i++) {
+      if(readStatus[i].checked) {
+         return readStatus[i].value;
+      }
+   }
+}
+
+
       
